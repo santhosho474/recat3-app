8017daa4800d8ea738278bedfc8a4e9ef924daa2
santhoshprince1 <santhoshreddy.sr48@gmail.com>
2021-05-31 17:07:57 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <MyComponent></MyComponent>
+      <br />
+      <MySecondComponent />
+      <br />
+      <MyThirdComponent />
+      <br />
+      <MyDyanamicInputCompoent />
     </div>
   );
 }
 
-export default App;
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>React Heading</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+        dolore culpa delectus iusto nemo nobis, suscipit, cupiditate quam
+        tempore quisquam nesciunt veritatis? Nostrum consequatur aperiam magnam
+        at optio mollitia enim.
+      </div>
+      <h4>Heading 4</h4>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        voluptatum ad quisquam reprehenderit doloremque. At id doloribus
+        exercitationem consectetur quod temporibus? Dolorem illum voluptas
+        eligendi non necessitatibus pariatur nobis quae.
+      </div>
+    </div>
+  );
+};
+
+const MySecondComponent = () => {
+  return (
+    <div>
+      <h1>My Second Component</h1>
+    </div>
+  );
+};
+
+const MyThirdComponent = () => {
+  let localCounter = 100;
+  let [counter, setCounter] = useState(100);
+  const updateCounter = () => {
+    localCounter = localCounter + 1;
+    setCounter(counter + 1);
+  };
+  return (
+    <div>
+      <h1>Third Component</h1>
+      <input type="button" value="Update Counter" onClick={updateCounter} />
+      <input
+        type="button"
+        value="Upadate counter1"
+        onClick={() => updateCounter()}
+      />
+      <div>React Not Aware -{localCounter}</div>
+      <div>React Aware -{counter}</div>
+    </div>
+  );
+};
+
+const MyDyanamicInputCompoent = () => {
+  const [list, setList] = useState([]);
+  const [tweet, setTweet] = useState("");
+
+  const readAndUpdateTweet = (e) => {
+    setTweet(e.target.value);
+  };
+  const addNewItem = () => {
+    setList([tweet, ...list]);
+    setTweet("");
+  };
+  return (
+    <div>
+      <head>Tweets</head>
+      <div>
+        <input
+          type="text"
+          value={tweet}
+          onChange={(e) => readAndUpdateTweet(e)}
+          style={{
+            cursor: "pointer",
+            paddingLeft: "8px",
+            borderRadius: "8px",
+            color: "royalblue",
+          }}
+          placeholder="Message..."
+        />
+        <input type="button" onClick={addNewItem} value="Tweet here" />
+      </div>
+      {list.map((item, index) => (
+        <div key={index} className="bg-primary">
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
